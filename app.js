35ae142bfdb83a37ee6fd20af8c2c5b81c0da199
stevewfogarty <steve.fogarty@gmail.com>
2023-02-09 19:42:07 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,91 @@
+//jshint esversion:6
+
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+//console.log(process.env.DB_HOST);
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+
+app.post("/register", function(req, res){
+
+     const userName = req.body.username;
+     const password = req.body.password;
+
+     const newUser = new User({
+        email: userName,
+        password: password
+     })
+
+     newUser.save(function(err){
+        if(!err) {
+            res.render("secrets");
+        }else {
+            res.send(err);
+        }
+     });
+});
+
+app.post("/login", function(req, res) {
+    const userName = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: userName}, function(err, userFound){
+        if(!userFound) {
+            res.send("User not found");
+
+        }else if(password != userFound.password) {
+            res.send("wrong password");
+        }
+        else{
+            
+            res.render("secrets");
+        }
+    });
+
+});
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
