8721d68e9f8a86be2ade71a460e8ac6e729c485b
stevewfogarty <steve.fogarty@gmail.com>
2023-02-11 13:42:21 -0400

    added secret submit functionality

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -61,12 +62,49 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+
+    User.find({secret: {$ne: null}}, function(err, foundUsers){
+        if(err) {
+            console.log(err);
+
+        }else {
+            if (foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
+    
+});
+
+
+app.get("/submit", function(req, res){
+    
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     }else{
         res.redirect("/login");
     }
 })
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+
+    user = User.findById(req.user.id,function(err, userFound){
+        if(err) {
+            console.log(err);
+        } else {
+            if(userFound) {
+                userFound.secret = submittedSecret;
+                userFound.save(function(){
+                    res.redirect("/secrets");
+                })
+            }
+        }
+    })
+    })
+
+
+
 
 app.get("/logout", function(req, res, next){
     req.logout(function(err){
